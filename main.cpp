07cddea02f3c30dffe3fa753e376930fdfda3008
matkatmusic <matkatmusic@gmail.com>
2020-03-15 19:01:20 +0000

    revised instructions

--- main.cpp
+++ main.cpp
@@ -7,25 +7,32 @@ Video:  Chapter 2 Part 5
  User-Defined Types
 
  Create a branch named Part1
+
+ Purpose: The entire purpose of this 5-part project is to get you writing C++ code that compiles and to reinforce the syntax habits that C++ requires.  What you create in this project will be used as the basis of Project 5 in the course.   
  
  1) write 10 user-defined types, each with a random number of member variables
-    try to make the member variables have names that are related to the user-defined type.
+    make the member variables have names that are related to type of work the UDT would perform.
+    see the example below for clarity on this instruction
  
  2) give the member variables relevant data types
  
- 3) add a couple member functions.  
+ 3) add at least 2 member functions to each of your 10 types.  
     make the function parameter list for those member functions use some of your User-Defined Types
+    You'll write definitions in part2 for these functions
+    you'll call each of these functions in part3
  
- 4) make 2 of the 10 user-defined types be nested class
+ 4) make 2 of the 10 user-defined types have a nested class.  this nested class also needs member functions.
  
- 5) One of your UDTs should only use UDTs for its member variable types.   
+ 5) One of your 10 UDTs should only use UDTs for its member variable types.   
      No primitives allowed!
  
  6) After you finish defining each type, click the [run] button.  Clear up any errors or warnings as best you can.
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
- Send me the the link to your repl.it in a DM on Slack
+ Make a pull request after you make your first commit and pin the pull request link to our DM thread.
+
+send me a DM to check your pull request
 
  Wait for my code review.
  */
